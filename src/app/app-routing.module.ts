e03492312745b819403e92b781a59b55c84f3489
Eloic Rakoto-Arisoa <eloicmax@gmail.com>
2025-04-02 16:30:25 +0300

    New update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,45 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginclientComponent } from './components/loginclient/loginclient.component';
-// import { ServiceComponent } from './service/service.component';
-// import { MecanicienComponent } from './mecanicien/mecanicien.component';
-// import { CongeComponent } from './conge/conge.component';
-// import { ListeServiceComponent } from './liste-service/liste-service.component';
-// import { EvenementComponent } from './evenement/evenement.component';
-// import { PromotionComponent } from './promotion/promotion.component';
-// import { PlanningComponent } from './planning/planning.component';
-// import { RendezvousComponent } from './rendezvous/rendezvous.component';
-// import { TableaubordComponent } from './tableaubord/tableaubord.component';
-// import { PrendrdvComponent } from './prendrdv/prendrdv.component';
-// import { OffresComponent } from './offres/offres.component';
-// import { ConseilTechComponent } from './conseil-tech/conseil-tech.component';
-// import { EntretienComponent } from './entretien/entretien.component';
-// import { DevisComponent } from './devis/devis.component';
-// import { ProfitComponent } from './profit/profit.component';
-
-
+import { ValidationrendezvousComponent } from './components/validationrendezvous/validationrendezvous.component';
+import { ServiceComponent } from './components/service/service.component';
+import { ValidationdemandeComponent } from './components/validationdemande/validationdemande.component';
+import { EvenementComponent } from './components/evenement/evenement.component';
 
 
 
 
 const routes: Routes = [
 
-// { path: 'service', component: ServiceComponent },
-// { path: 'mecanicien', component: MecanicienComponent},
-// { path: 'conge', component: CongeComponent},
-// { path: 'listService', component: ListeServiceComponent},
-// { path: 'evenement', component: EvenementComponent },
-// { path: 'promotion', component : PromotionComponent},
-// { path: 'planning', component: PlanningComponent},
-// { path: 'rendezvous', component: RendezvousComponent},
-// { path: 'dashboard', component: TableaubordComponent},
-// { path: 'rdv', component: PrendrdvComponent},
-// { path: 'offre', component: OffresComponent},
-// { path: 'conseiltech', component: ConseilTechComponent},
-// { path: 'entretien', component: EntretienComponent},
-// { path: 'devis', component: DevisComponent},
-// { path: 'profit', component: ProfitComponent}
-
+{ path: 'validationrendezvous', component: ValidationrendezvousComponent },
+{ path: 'service', component: ServiceComponent },
+{ path: 'validationdemande', component: ValidationdemandeComponent },
+{ path: 'evenement', component: EvenementComponent },
+// { path: 'dashboard', component: EvenementComponent },
 
 
 ];
