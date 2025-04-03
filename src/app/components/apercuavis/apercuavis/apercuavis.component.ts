a769a66f0a3994d618c36994c0106361348f90b1
Eloic Rakoto-Arisoa <eloicmax@gmail.com>
2025-04-03 11:44:37 +0300

    Update avis

--- src/app/components/apercuavis/apercuavis/apercuavis.component.ts
+++ src/app/components/apercuavis/apercuavis/apercuavis.component.ts
@@ -42,7 +42,6 @@ export class ApercuavisComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
-    alert(this.id);
     this.loadAvisClient();
   }
 
